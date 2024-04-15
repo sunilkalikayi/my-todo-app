60dd3fcec0e9c5459d857fb05f3c428aef6cb8ad
sunilkalikayi2580@gmail.com <sunilkalikayi2580@gmail.com>
2024-04-15 13:01:29 +0530

    feat: tabs layout is done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/Navbar";
 import SidebarItem from "@/components/SidebarItem";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Inter } from "next/font/google";
 import CalendarIcon from "../../public/icons/CalendarIcon";
 
@@ -7,14 +8,26 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
     return (
-        <main className={`min-h-screen  ${inter.className}`}>
+        <main className={`${inter.className} min-h-screen flex flex-col`}>
             {/* Nav bar */}
             <Navbar />
-            {/* side bar */}
-            <div>
-                <SidebarItem icon={<CalendarIcon />} text={"Today"} count={"3 / 7"} />
-            </div>
             {/* body */}
+            <Tabs defaultValue="account" className=" flex-1 flex flex-row">
+                {/* side bar */}
+                <TabsList className="flex-col bg-[red] max-w-72">
+                    <TabsTrigger value="account">
+                        <SidebarItem icon={<CalendarIcon />} text={"Today"} count={"3 / 7"} />
+                    </TabsTrigger>
+                    <TabsTrigger value="password">
+                        <SidebarItem icon={<CalendarIcon />} text={"Today"} count={"3 / 7"} />
+                    </TabsTrigger>
+                </TabsList>
+                {/* Tab pannel */}
+                <div className="bg-[green] flex-1">
+                    <TabsContent value="account">Make changes to your account here.</TabsContent>
+                    <TabsContent value="password">Change your password here.</TabsContent>
+                </div>
+            </Tabs>
         </main>
     );
 }
